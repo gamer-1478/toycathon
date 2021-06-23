2c39959f034dffedb69ea07aa7e37f4a159703cd
gigabite-pro <rajmachawal19@gmail.com>
2021-06-23 10:46:54 +0530

    money added

--- lib/screens/mainGame.dart
+++ lib/screens/mainGame.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'homepage.dart';
+import 'package:bharat_mystery/screens/homepage.dart' as HomePage;
 
 class MainGame extends StatefulWidget {
   final List players;
@@ -60,7 +60,13 @@ class _MainGameState extends State<MainGame> {
               //yes
               TextButton(
                 onPressed: () {
-                  HomePage();
+                  widget.players.clear();
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              HomePage.HomePage()),
+                      (Route<dynamic> route) => false);
                 },
                 child: Text('Yes'),
               ),
@@ -81,9 +87,22 @@ class _MainGameState extends State<MainGame> {
     var p1I = nameparts1[0][0].toUpperCase() + nameparts1[1][0].toUpperCase();
     player1Initials = p1I;
     _setListFromSharedPref();
+    if (nameparts1.length == 2) {
+      var p1I = nameparts1[0][0].toUpperCase() + nameparts1[1][0].toUpperCase();
+      player1Initials = p1I;
+    } else {
+      var p1I = nameparts1[0][0].toUpperCase();
+      player1Initials = p1I;
+    }
+
     var nameparts2 = widget.players[1].split(" ");
-    var p2I = nameparts2[0][0].toUpperCase() + nameparts2[1][0].toUpperCase();
-    player2Initials = p2I;
+    if (nameparts2.length == 2) {
+      var p2I = nameparts2[0][0].toUpperCase() + nameparts2[1][0].toUpperCase();
+      player2Initials = p2I;
+    } else {
+      var p2I = nameparts2[0][0].toUpperCase();
+      player2Initials = p2I;
+    }
   }
 
   int newDiceImage = 1;
@@ -94,174 +113,201 @@ class _MainGameState extends State<MainGame> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: _onWillPop,
-      child: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
-            child: Column(
-              children: <Widget>[
-                //players
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: activeColor1,
-                      radius: 50,
-                      child: CircleAvatar(
-                        backgroundColor: Theme.of(context).highlightColor,
-                        radius: 45,
-                        child: Text(
-                          player1Initials,
-                          style: TextStyle(
-                            fontFamily: 'Lexend Deca',
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: Theme.of(context).accentColor,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
+              child: Column(
+                children: <Widget>[
+                  //players
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: activeColor1,
+                        radius: 50,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 45,
+                          child: Text(
+                            player1Initials,
+                            style: TextStyle(
+                              fontFamily: 'LexendDeca',
+                              color: Colors.black,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    CircleAvatar(
-                      backgroundColor: activeColor2,
-                      radius: 50,
-                      child: CircleAvatar(
-                        backgroundColor: Theme.of(context).highlightColor,
-                        radius: 45,
-                        child: Text(
-                          player2Initials,
-                          style: TextStyle(
-                            fontFamily: 'Lexend Deca',
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
+                      CircleAvatar(
+                        backgroundColor: activeColor2,
+                        radius: 50,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 45,
+                          child: Text(
+                            player2Initials,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'LexendDeca',
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10.0)),
-                      height: 100.0,
-                      width: 100.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset('assets/Dice-$newDiceImage.png'),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        '₹ 1500',
+                        style: TextStyle(
+                          color: Theme.of(context).highlightColor,
+                          fontFamily: 'LexendDeca',
+                          fontSize: 17.0,
+                        ),
                       ),
+                      Text(
+                        '₹ 1500',
+                        style: TextStyle(
+                          color: Theme.of(context).highlightColor,
+                          fontFamily: 'LexendDeca',
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0)),
+                        height: 100.0,
+                        width: 100.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset('assets/Dice-$newDiceImage.png'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        newDiceImage = Random().nextInt(6) + 1;
+                        if (activeColor1 == Colors.blue) {
+                          activeColor1 = Colors.black;
+                          activeColor2 = Colors.blue;
+                          activePlayer = true;
+                          _setListFromSharedPref();
+                        } else {
+                          activeColor1 = Colors.blue;
+                          activeColor2 = Colors.black;
+                          activePlayer = false;
+                          _setListFromSharedPref();
+                        }
+                      });
+                    },
+                    height: 40.0,
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    shape: StadiumBorder(),
+                    child: Text(
+                      "Roll Dice",
+                      style: TextStyle(
+                          fontFamily: 'LexendDeca',
+                          fontSize: 16.0,
+                          color: Theme.of(context).cardColor),
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    setState(() {
-                      newDiceImage = Random().nextInt(6) + 1;
-                      if (activeColor1 == Colors.blue) {
-                        activeColor1 = Colors.black;
-                        activeColor2 = Colors.blue;
-                        activePlayer = true;
-                        _setListFromSharedPref();
-                      } else {
-                        activeColor1 = Colors.blue;
-                        activeColor2 = Colors.black;
-                        activePlayer = false;
-                        _setListFromSharedPref();
-                      }
-                    });
-                  },
-                  height: 40.0,
-                  padding: EdgeInsets.symmetric(horizontal: 30.0),
-                  shape: StadiumBorder(),
-                  child: Text(
-                    "Roll Dice",
-                    style: TextStyle(
-                        fontFamily: 'LexendDeca',
-                        fontSize: 16.0,
-                        color: Theme.of(context).cardColor),
                   ),
-                  color: Theme.of(context).highlightColor,
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
 
-                MaterialButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AvailableCards(),
-                        ));
-                  },
-                  height: 50.0,
-                  padding: EdgeInsets.symmetric(horizontal: 40.0),
-                  shape: StadiumBorder(),
-                  child: Text(
-                    "Available Monuments",
-                    style: TextStyle(
-                        fontFamily: 'LexendDeca',
-                        fontSize: 16.0,
-                        color: Theme.of(context).cardColor),
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AvailableCards(),
+                          ));
+                    },
+                    height: 50.0,
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    shape: StadiumBorder(),
+                    child: Text(
+                      "Available Monuments",
+                      style: TextStyle(
+                          fontFamily: 'LexendDeca',
+                          fontSize: 16.0,
+                          color: Theme.of(context).cardColor),
+                    ),
+                    color: Theme.of(context).highlightColor,
                   ),
-                  color: Theme.of(context).highlightColor,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
 
-                MaterialButton(
-                  onPressed: () {
-                    setState(() {
-                      newDiceImage = Random().nextInt(6) + 1;
-                    });
-                  },
-                  height: 50.0,
-                  padding: EdgeInsets.symmetric(horizontal: 40.0),
-                  shape: StadiumBorder(),
-                  child: Text(
-                    "My Monuments",
-                    style: TextStyle(
-                        fontFamily: 'LexendDeca',
-                        fontSize: 16.0,
-                        color: Theme.of(context).cardColor),
+                  MaterialButton(
+                    onPressed: () {
+                      setState(() {
+                        newDiceImage = Random().nextInt(6) + 1;
+                      });
+                    },
+                    height: 50.0,
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    shape: StadiumBorder(),
+                    child: Text(
+                      "My Monuments",
+                      style: TextStyle(
+                          fontFamily: 'LexendDeca',
+                          fontSize: 16.0,
+                          color: Theme.of(context).cardColor),
+                    ),
+                    color: Theme.of(context).highlightColor,
                   ),
-                  color: Theme.of(context).highlightColor,
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
 
-                MaterialButton(
-                  onPressed: () {
-                    _endGame();
-                  },
-                  height: 50.0,
-                  padding: EdgeInsets.symmetric(horizontal: 40.0),
-                  shape: StadiumBorder(),
-                  child: Text(
-                    "End Game",
-                    style: TextStyle(
-                        fontFamily: 'LexendDeca',
-                        fontSize: 16.0,
-                        color: Theme.of(context).cardColor),
+                  MaterialButton(
+                    onPressed: () {
+                      _endGame();
+                    },
+                    height: 50.0,
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    shape: StadiumBorder(),
+                    child: Text(
+                      "End Game",
+                      style: TextStyle(
+                          fontFamily: 'LexendDeca',
+                          fontSize: 16.0,
+                          color: Theme.of(context).cardColor),
+                    ),
+                    color: Theme.of(context).highlightColor,
                   ),
-                  color: Theme.of(context).highlightColor,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
